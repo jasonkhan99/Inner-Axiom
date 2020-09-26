622b7c8991f1893162a75ee2827cf8c0e553a4d2
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-26 10:15:18 -0700

    create cameracontrol script

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public float speed;
-
-    void Update ()
+    public GameObject targetObject;
+    private float targetAngle = 0;
+    const float rotationAmount = 1.5f;
+    public float rDistance = 1.0f;
+    public float rSpeed = 1.0f;
+    
+    // Update is called once per frame
+    void Update()
     {
-        transform.Rotate(0, speed * Time.deltaTime, 0);
+    
+        // Trigger functions if Rotate is requested
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            targetAngle -= 90.0f;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            targetAngle += 90.0f;
+        }
+    
+        if(targetAngle !=0)
+        {
+            Rotate();
+        }
+    }
+    
+    protected void Rotate()
+    {
+
+        float step = rSpeed * Time.deltaTime;
+        float orbitCircumfrance = 2F * rDistance * Mathf.PI;
+        float distanceDegrees = (rSpeed / orbitCircumfrance) * 360;
+        float distanceRadians = (rSpeed / orbitCircumfrance) * 2 * Mathf.PI;
+        
+        if (targetAngle>0)
+        {
+            transform.RotateAround(targetObject.transform.position, Vector3.up, -rotationAmount);
+            targetAngle -= rotationAmount;
+        }
+        else if(targetAngle <0)
+        {
+            transform.RotateAround(targetObject.transform.position, Vector3.up, rotationAmount);
+            targetAngle += rotationAmount;
+        }
+    
     }
 }
