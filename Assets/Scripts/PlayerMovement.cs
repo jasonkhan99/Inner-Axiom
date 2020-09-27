4043a0be8881973d229542aab7a265ef21a61198
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-26 17:58:20 -0700

    playermovement extends charactermovement, call findselectable and init

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : CharacterMovement
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        findSelectableTiles();
     }
 }
