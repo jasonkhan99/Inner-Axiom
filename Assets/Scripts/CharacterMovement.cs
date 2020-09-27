39fe40a897fd18d6a02bfbd7ff7d5847b786cdf7
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-26 17:12:08 -0700

    setup variables for charmove class

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<Tile> selectableTiles = new List<Tile>();
+    GameObject[] tiles;
+    public int move = 5;
+    public float jumpHeight = 1;
+    Vector3 velocity = new Vector3();
+    Vector3 heading = new Vector3();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
