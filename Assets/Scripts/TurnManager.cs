083f192ed0902896159ce21ab989cfa187fb4613
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-29 20:17:19 -0700

    add turn manager script

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    static Dictionary<string, List<CharacterMovement>> units = new Dictionary<string, List<CharacterMovement>>();
+    static Queue<string> turnKey = new Queue<string>();
+    static Queue<CharacterMove> turnTeam = new Queue<CharacterMove>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turnTeam.Count == 0)
+        {
+            InitTeamTurnQueue();
+        }
+    }
+
+    static void InitTeamTurnQueue()
+    {
+        List<CharacterMove> teamList = units[turnKey.Peek()];
+        
+        foreach (CharacterMove unit in teamList)
+        {
+            turnTeam.Enqueue(unit);
+        }
+    }
+}
