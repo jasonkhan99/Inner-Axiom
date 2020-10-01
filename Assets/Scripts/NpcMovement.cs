651eb88ab4f106398d1e52b9e1c00e0945c6e543
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-30 17:32:58 -0700

    copy boilerplate from playermove

--- Assets/Scripts/NpcMovement.cs
+++ Assets/Scripts/NpcMovement.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcMovement : MonoBehaviour
+public class NpcMovement : CharacterMovement
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(transform.position, transform.forward);
+
+        if (!turn)
+        {
+            return;
+        }
         
+        if (!moving)
+        {
+            FindSelectableTiles();
+        }
+        else
+        {
+            CharacterMove();
+        }
     }
 }
