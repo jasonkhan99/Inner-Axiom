2e2dbd489a5e05edd60f5a234e124bec5a3cc8ee
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 12:59:50 -0700

    create directions extentions

---
+++ Assets/Scripts/Extensions/DirectionsExtentions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+public static class DirectionsExtensions
+{
+    public static Directions GetDirection (this Tile t1, Tile t2)
+    {
+        if (t1.pos.y < t2.pos.y)
+        {
+            return Directions.North;
+        }
+        if (t1.pos.x < t2.pos.x)
+        {
+            return Directions.East;
+        }
+        if (t1.pos.y > t2.pos.y)
+        {
+            return Directions.South;
+        }
+        return Directions.West;
+    }
+    
+    public static Vector3 ToEuler (this Directions d)
+    {
+        return new Vector3(0, (int)d * 90, 0);
+    }
+}
