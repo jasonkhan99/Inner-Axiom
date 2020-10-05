d73d05d2978f7c14612080b0bc5e294e8442ca82
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 15:19:36 -0700

    update initbattlestate

--- Assets/Scripts/Controller/BattleStates/InitBattleState.cs
+++ Assets/Scripts/Controller/BattleStates/InitBattleState.cs
@@ -12,7 +12,27 @@ public class InitBattleState : BattleState
         board.Load( levelData );
         Point p = new Point((int)levelData.tiles[0].x, (int)levelData.tiles[0].z);
         SelectTile(p);
+        SpawnTestUnits();
         yield return null;
         owner.ChangeState<MoveTargetState>();
+    }
+
+    void SpawnTestUnits ()
+    {
+        System.Type[] components = new System.Type[]{ typeof(WalkMovement), typeof(FlyMovement), typeof(TeleportMovement) };
+        for (int i = 0; i < 3; ++i)
+        {
+            GameObject instance = Instantiate(owner.heroPrefab) as GameObject;
+
+            Point p = new Point((int)levelData.tiles[i].x, (int)levelData.tiles[i].z);
+
+            Unit unit = instance.GetComponent<Unit>();
+            unit.Place(board.GetTile(p));
+            unit.Match();
+            
+            Movement m = instance.AddComponent(components[i]) as Movement;
+            m.range = 5;
+            m.jumpHeight = 1;
+        }
     }
 }
