0d5a126ba9c788d4f23bed47725acdaecfeabd9a
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 10:47:34 -0700

    add board to vmc

---
+++ Assets/Scripts/ViewModelComponent/Board.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Board : MonoBehaviour 
+{
+    [SerializeField] GameObject tilePrefab;
+    public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
+
+    public void Load (LevelData data)
+    {
+        for (int i = 0; i < data.tiles.Count; ++i)
+        {
+            GameObject instance = Instantiate(tilePrefab) as GameObject;
+            Tile t = instance.GetComponent<Tile>();
+            t.Load(data.tiles[i]);
+            tiles.Add(t.pos, t);
+        }
+    }
+}
