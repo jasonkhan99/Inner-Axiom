3f81c10edcefa4be018be6d5d146b3b0ab9b28a6
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 16:34:49 -0700

    update panel.cs

--- Assets/Scripts/Common/UI/Panel.cs
+++ Assets/Scripts/Common/UI/Panel.cs
@@ -6,6 +6,14 @@ using System.Collections.Generic;
 [RequireComponent(typeof(LayoutAnchor))]
 public class Panel : MonoBehaviour 
 {
+    [SerializeField] List<Position> positionList;
+    Dictionary<string, Position> positionMap;
+    LayoutAnchor anchor;
+
+    public Position CurrentPosition { get; private set; }
+    public Tweener Transition { get; private set; }
+    public bool InTransition { get { return Transition != null; }}
+
     [Serializable]
     public class Position
     {
@@ -13,6 +21,18 @@ public class Panel : MonoBehaviour
         public TextAnchor myAnchor;
         public TextAnchor parentAnchor;
         public Vector2 offset;
+
+        public Position this[string name]
+        {
+            get
+            {
+                if (positionMap.ContainsKey(name))
+                {
+                    return positionMap[name];
+                }
+            return null;
+            }
+        }
         
         public Position (string name)
         {
@@ -29,5 +49,64 @@ public class Panel : MonoBehaviour
         {
             this.offset = offset;
         }
+    }
+
+    void Awake ()
+    {
+        anchor = GetComponent<LayoutAnchor>();
+        positionMap = new Dictionary<string, Position>(positionList.Count);
+        for (int i = positionList.Count - 1; i >= 0; --i)
+        {
+            AddPosition(positionList[i]);
+        }
+    }
+    
+    public void AddPosition (Position p)
+    {
+        positionMap[p.name] = p;
+    }
+
+    public void RemovePosition (Position p)
+    {
+        if (positionMap.ContainsKey(p.name))
+        {
+            positionMap.Remove(p.name);
+        }
+    }
+
+    public Tweener SetPosition (string positionName, bool animated)
+    {
+        return SetPosition(this[positionName], animated);
+    }
+
+    public Tweener SetPosition (Position p, bool animated)
+    {
+        CurrentPosition = p;
+        if (CurrentPosition == null)
+        {
+            return null;
+        }
+        if (InTransition)
+        {
+        Transition.easingControl.Stop();
+        }
+        if (animated)
+        {
+            Transition = anchor.MoveToAnchorPosition(p.myAnchor, p.parentAnchor, p.offset);
+            return Transition;
+        }
+        else
+        {
+            anchor.SnapToAnchorPosition(p.myAnchor, p.parentAnchor, p.offset);
+            return null;
+        }
+    }
+
+    void Start ()
+    {
+        if (CurrentPosition == null && positionList.Count > 0)
+            {
+            SetPosition(positionList[0], false);
+            }
     }
 }
