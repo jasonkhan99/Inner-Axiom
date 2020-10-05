ee113fc9af450b9623cde2936d1f3032bf01b49b
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 15:23:59 -0700

    update move target state

--- Assets/Scripts/Controller/BattleStates/MoveTargetState.cs
+++ Assets/Scripts/Controller/BattleStates/MoveTargetState.cs
@@ -1,9 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class MoveTargetState : BattleState 
+using System.Collections.Generic;
+public class MoveTargetState : BattleState
 {
+    List<Tile> tiles;
+    
+    public override void Enter ()
+    {
+        base.Enter ();
+        Movement mover = owner.currentUnit.GetComponent<Movement>();
+        tiles = mover.GetTilesInRange(board);
+        board.SelectTiles(tiles);
+    }
+    
+    public override void Exit ()
+    {
+        base.Exit ();
+        board.DeSelectTiles(tiles);
+        tiles = null;
+    }
+    
     protected override void OnMove (object sender, InfoEventArgs<Point> e)
     {
         SelectTile(e.info + pos);
+    }
+    
+    protected override void OnFire (object sender, InfoEventArgs<int> e)
+    {
+        if (tiles.Contains(owner.currentTile))
+        {
+            owner.ChangeState<MoveSequenceState>();
+        }
     }
 }
