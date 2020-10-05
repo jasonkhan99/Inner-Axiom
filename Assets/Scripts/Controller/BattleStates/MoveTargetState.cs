0cb5df415bbee9e2308b0e6308522d16322568b8
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 10:51:45 -0700

    add movetargetstate

---
+++ Assets/Scripts/Controller/BattleStates/MoveTargetState.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MoveTargetState : BattleState 
+{
+    protected override void OnMove (object sender, InfoEventArgs<Point> e)
+    {
+        SelectTile(e.info + pos);
+    }
+}
