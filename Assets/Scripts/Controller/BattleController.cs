8ccbcd217951667beeb76eb98b535173835c3452
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 09:44:55 -0700

    add battle controller.cs

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class BattleController : StateMachine 
+{
+    public CameraRig cameraRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform tileSelectionIndicator;
+    public Point pos;
+    void Start ()
+    {
+        ChangeState<InitBattleState>();
+    }
+}
