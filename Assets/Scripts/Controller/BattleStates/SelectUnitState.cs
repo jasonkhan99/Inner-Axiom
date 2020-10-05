aca5e507fdaa8c49f0332723e3cd8abc9df1c2a4
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 15:22:26 -0700

    create select unit state

---
+++ Assets/Scripts/Controller/BattleStates/SelectUnitState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SelectUnitState : BattleState 
+{
+    protected override void OnMove (object sender, InfoEventArgs<Point> e)
+    {
+        SelectTile(e.info + pos);
+    }
+    
+    protected override void OnFire (object sender, InfoEventArgs<int> e)
+    {
+        GameObject content = owner.currentTile.content;
+        if (content != null)
+        {
+            owner.currentUnit = content.GetComponent<Unit>();
+            owner.ChangeState<MoveTargetState>();
+        }
+    }
+}
