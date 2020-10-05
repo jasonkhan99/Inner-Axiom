77c9c845ce261df9d7900855d5293b2a89fc9b2d
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 10:50:45 -0700

    add initbattlestate

---
+++ Assets/Scripts/Controller/BattleStates/InitBattleState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class InitBattleState : BattleState 
+{
+    public override void Enter ()
+    {
+        base.Enter ();
+        StartCoroutine(Init());
+    }
+    IEnumerator Init ()
+    {
+        board.Load( levelData );
+        Point p = new Point((int)levelData.tiles[0].x, (int)levelData.tiles[0].z);
+        SelectTile(p);
+        yield return null;
+        owner.ChangeState<MoveTargetState>();
+    }
+}
