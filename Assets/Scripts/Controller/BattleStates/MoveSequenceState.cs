f866e759d7da9cb85c67a10703b5b9370f66ee18
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 15:25:43 -0700

    create move sequence state

---
+++ Assets/Scripts/Controller/BattleStates/MoveSequenceState.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MoveSequenceState : BattleState 
+{
+    public override void Enter ()
+    {
+        base.Enter ();
+        StartCoroutine("Sequence");
+    }
+    
+    IEnumerator Sequence ()
+    {
+        Movement m = owner.currentUnit.GetComponent<Movement>();
+        yield return StartCoroutine(m.Traverse(owner.currentTile));
+        owner.ChangeState<SelectUnitState>();
+    }
+}
