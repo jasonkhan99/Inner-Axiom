741474c07381acdbb116dd6c8ab10b866cd55e60
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 13:24:31 -0700

    create walkmovement

---
+++ Assets/Scripts/ViewModelComponent/Movement/Movement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public abstract class Movement : MonoBehaviour
+{
+    public int range;
+    public int jumpHeight;
+    protected Unit unit;
+    protected Transform jumper;
+
+    public abstract IEnumerator Traverse (Tile tile);
+
+
+    protected virtual void Awake ()
+    {
+        unit = GetComponent<Unit>();
+        jumper = transform.FindChild("Jumper");
+    }
+
+    public virtual List<Tile> GetTilesInRange (Board board)
+    {
+        List<Tile> retValue = board.Search( unit.tile, ExpandSearch );
+        Filter(retValue);
+        return retValue;
+    }
+
+    protected virtual bool ExpandSearch (Tile from, Tile to)
+    {
+        return (from.distance + 1) <= range;
+    }
+
+    protected virtual void Filter (List<Tile> tiles)
+    {
+        for (int i = tiles.Count - 1; i >= 0; --i)
+        {
+            if (tiles[i].content != null)
+            {
+                tiles.RemoveAt(i);
+            }
+        }
+    }
+
+    protected virtual IEnumerator Turn (Directions dir)
+    {
+        TransformLocalEulerTweener t = (TransformLocalEulerTweener)transform.RotateToLocal(dir.ToEuler(), 0.25f, EasingEquations.EaseInOutQuad);
+    
+        // When rotating between North and West, we must make an exception so it looks like the unit
+        // rotates the most efficient way (since 0 and 360 are treated the same)
+        if (Mathf.Approximately(t.startValue.y, 0f) && Mathf.Approximately(t.endValue.y, 270f))
+        {
+            t.startValue = new Vector3(t.startValue.x, 360f, t.startValue.z);
+        }
+        else if (Mathf.Approximately(t.startValue.y, 270) && Mathf.Approximately(t.endValue.y, 0))
+        {
+            t.endValue = new Vector3(t.startValue.x, 360f, t.startValue.z);
+        }
+        unit.dir = dir;
+    
+        while (t != null)
+        {
+            yield return null;
+        }
+    }
+}
+
---
+++ Assets/Scripts/ViewModelComponent/Movement/WalkMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    protected override bool ExpandSearch (Tile from, Tile to)
+    {
+    // Skip if the distance in height between the two tiles is more than the unit can jump
+        if ((Mathf.Abs(from.height - to.height) > jumpHeight))
+        {
+            return false;
+        }
+        // Skip if the tile is occupied by an enemy
+        if (to.content != null)
+        {
+            return false;
+        }
+        return base.ExpandSearch(from, to);
+    }
+
+    public override IEnumerator Traverse (Tile tile)
+    {
+        unit.Place(tile);
+        // Build a list of way points from the unit's 
+        // starting tile to the destination tile
+        List<Tile> targets = new List<Tile>();
+        while (tile != null)
+        {
+            targets.Insert(0, tile);
+            tile = tile.prev;
+        }
+        // Move to each way point in succession
+        for (int i = 1; i < targets.Count; ++i)
+        {
+            Tile from = targets[i-1];
+            Tile to = targets[i];
+            Directions dir = from.GetDirection(to);
+            if (unit.dir != dir)
+            yield return StartCoroutine(Turn(dir));
+            if (from.height == to.height)
+            yield return StartCoroutine(Walk(to));
+            else
+            yield return StartCoroutine(Jump(to));
+        }
+        yield return null;
+    }
+
+    IEnumerator Walk (Tile target)
+    {
+        Tweener tweener = transform.MoveTo(target.center, 0.5f, EasingEquations.Linear);
+        while (tweener != null)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator Jump (Tile to)
+    {
+        Tweener tweener = transform.MoveTo(to.center, 0.5f, EasingEquations.Linear);
+        Tweener t2 = jumper.MoveToLocal(new Vector3(0, Tile.stepHeight * 2f, 0), tweener.easingControl.duration / 2f, EasingEquations.EaseOutQuad);
+        t2.easingControl.loopCount = 1;
+        t2.easingControl.loopType = EasingControl.LoopType.PingPong;
+        while (tweener != null)
+        {
+            yield return null;
+        }
+    }
+}
