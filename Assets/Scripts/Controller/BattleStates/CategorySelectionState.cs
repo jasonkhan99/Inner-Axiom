6f7a17bdcad66942665e828a43d02aa33b3edaaf
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 11:32:51 -0700

    create category selection state

---
+++ Assets/Scripts/Controller/BattleStates/CategorySelectionState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class CategorySelectionState : BaseAbilityMenuState 
+{
+    protected override void LoadMenu ()
+    {
+        if (menuOptions == null)
+        {
+            menuTitle = "Action";
+            menuOptions = new List<string>(3);
+            menuOptions.Add("Attack");
+            menuOptions.Add("White Magic");
+            menuOptions.Add("Black Magic");
+        }
+        
+        abilityMenuPanelController.Show(menuTitle, menuOptions);
+    }
+
+    protected override void Confirm ()
+    {
+        switch (abilityMenuPanelController.selection)
+        {
+            case 0:
+                Attack();
+                break;
+            case 1:
+                SetCategory(0);
+                break;
+            case 2:
+                SetCategory(1);
+                break;
+        }
+    }
+
+    protected override void Cancel ()
+    {
+        owner.ChangeState<CommandSelectionState>();
+    }
+
+    void Attack ()
+    {
+        turn.hasUnitActed = true;
+        if (turn.hasUnitMoved)
+        {
+            turn.lockMove = true;
+        }
+        owner.ChangeState<CommandSelectionState>();
+    }
+
+    void SetCategory (int index)
+    {
+        ActionSelectionState.category = index;
+        owner.ChangeState<ActionSelectionState>();
+    }
+}
+
