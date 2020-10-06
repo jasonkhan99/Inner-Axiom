31596e334ea32e6347a7ac992e2ef8e384263de2
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-05 18:48:55 -0700

    create asset creator

---
+++ Assets/Editor/AssetCreator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class YourClassAsset
+{
+    [MenuItem("Assets/Create/Conversation Data")]
+    public static void CreateConversationData ()
+    {
+        ScriptableObjectUtility.CreateAsset<ConversationData> ();
+    }
+}
