3f05da5614f62f63cd7a8a12fcdc8be405278658
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 18:23:44 -0700

    add different subclass modifiers

---
+++ Assets/Scripts/Exceptions/Modifiers/AddValueModifier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class AddValueModifier : ValueModifier
+{
+    public readonly float toAdd;
+
+    public AddValueModifier (int sortOrder, float toAdd) : base (sortOrder)
+    {
+        this.toAdd = toAdd;
+    }
+    
+    public override float Modify (float value)
+    {
+        return value + toAdd;
+    }
+}
---
+++ Assets/Scripts/Exceptions/Modifiers/MaxValueModifier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MaxValueModifier : ValueModifier 
+{
+    public float max;
+
+    public MaxValueModifier (int sortOrder, float max) : base (sortOrder)
+    {
+        this.max = max;
+    }
+    
+    public override float Modify (float value)
+    {
+        return Mathf.Max(value, max);
+    }
+}
