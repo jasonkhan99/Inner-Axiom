cdafbd31aca94e85f51e0afe6e7ee16a58458879
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 18:40:39 -0700

    add stats.cs

---
+++ Assets/Scripts/ViewModelComponent/Actor/Stats.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stats : MonoBehaviour 
+{
+    public int this[StatTypes s]
+    {
+        get { return _data[(int)s]; }
+        set { SetValue(s, value, true); }
+    }
+    int[] _data = new int[ (int)StatTypes.Count ];
+
+    public static string WillChangeNotification (StatTypes type)
+    {
+        if (!_willChangeNotifications.ContainsKey(type))
+        {
+            _willChangeNotifications.Add(type, string.Format("Stats.{0}WillChange", type.ToString()));
+        }
+        return _willChangeNotifications[type];
+    }
+
+    public static string DidChangeNotification (StatTypes type)
+    {
+        if (!_didChangeNotifications.ContainsKey(type))
+        {
+            _didChangeNotifications.Add(type, string.Format("Stats.{0}DidChange", type.ToString()));
+        }
+        return _didChangeNotifications[type];
+    }
+
+    static Dictionary<StatTypes, string> _willChangeNotifications = new Dictionary<StatTypes, string>();
+    static Dictionary<StatTypes, string> _didChangeNotifications = new Dictionary<StatTypes, string>();
+
+    public void SetValue (StatTypes type, int value, bool allowExceptions)
+    {
+        int oldValue = this[type];
+        if (oldValue == value)
+        {
+            return;
+        }
+    
+        if (allowExceptions)
+        {
+            // Allow exceptions to the rule here
+            ValueChangeException exc = new ValueChangeException( oldValue, value );
+            
+            // The notification is unique per stat type
+            this.PostNotification(WillChangeNotification(type), exc);
+            
+            // Did anything modify the value?
+            value = Mathf.FloorToInt(exc.GetModifiedValue());
+            
+            // Did something nullify the change?
+            if (exc.toggle == false || value == oldValue)
+            {
+                return;
+            }
+        }
+        _data[(int)type] = value;
+        this.PostNotification(DidChangeNotification(type), oldValue);
+    }
+}
