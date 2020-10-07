4ce1356a38fa14b0846db532b03dcbca86547da3
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 18:29:29 -0700

    create value change exception

---
+++ Assets/Scripts/Exceptions/ValueChangeException.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ValueChangeException : BaseException
+{
+    #region Fields / Properties
+    public readonly float fromValue;
+    public readonly float toValue;
+    public float delta { get { return toValue - fromValue; }}
+    List<ValueModifier> modifiers;
+    #endregion
+
+    #region Constructor
+    public ValueChangeException (float fromValue, float toValue) : base (true)
+    {
+        this.fromValue = fromValue;
+        this.toValue = toValue;
+    }
+    #endregion
+
+    #region Public
+    public void AddModifier (ValueModifier m)
+    {
+        if (modifiers == null)
+        {
+            modifiers = new List<ValueModifier>();
+        }
+        modifiers.Add(m);
+    }
+
+    public float GetModifiedValue ()
+    {
+        float value = toValue;
+        if (modifiers == null)
+        {
+            return value;
+        }
+        
+        modifiers.Sort(Compare);
+        for (int i = 0; i < modifiers.Count; ++i)
+        {
+            value = modifiers[i].Modify(value);
+        }
+        
+        return value;
+    }
+    #endregion
+    
+    #region Private
+    int Compare (ValueModifier x, ValueModifier y)
+    {
+        return x.sortOrder.CompareTo(y.sortOrder);
+    }
+    #endregion  
+}
