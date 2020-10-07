4de350ac13f9593eba78f49e4b2bee08db54606a
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 19:22:55 -0700

    add equippable.cs

---
+++ Assets/Scripts/ViewModelComponent/Item/Equippable.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Equippable : MonoBehaviour 
+{
+    #region Fields
+    public EquipSlots defaultSlots;
+    public EquipSlots secondarySlots;
+    public EquipSlots slots;
+    bool _isEquipped;
+    #endregion
+
+    #region Public
+    public void OnEquip ()
+    {
+        if (_isEquipped)
+        {
+            return;
+        }
+
+        _isEquipped = true;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; ++i)
+        {
+            features[i].Activate(gameObject);
+        }
+    }
+
+    public void OnUnEquip ()
+    {
+        if (!_isEquipped)
+        {
+            return;
+        }
+
+        _isEquipped = false;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; ++i)
+        {
+            features[i].Deactivate();
+        }
+    }
+    #endregion
+}
