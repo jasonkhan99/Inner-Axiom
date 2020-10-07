8509b6131ae8e6bc98955a267ec8dad7a9eb7790
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 23:06:49 -0700

    create status effects

---
+++ Assets/Scripts/ViewModelComponent/Status/Effects/StopStatusEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopStatusEffect : StatusEffect 
+{
+	Stats myStats;
+
+	void OnEnable ()
+	{
+		myStats = GetComponentInParent<Stats>();
+		if (myStats)
+			this.AddObserver( OnCounterWillChange, Stats.WillChangeNotification(StatTypes.CTR), myStats );
+	}
+	
+	void OnDisable ()
+	{
+		this.RemoveObserver( OnCounterWillChange, Stats.WillChangeNotification(StatTypes.CTR), myStats );
+	}
+	
+	void OnCounterWillChange (object sender, object args)
+	{
+		ValueChangeException exc = args as ValueChangeException;
+		exc.FlipToggle();
+	}
+}
