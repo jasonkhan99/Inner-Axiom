09d8cb2f3bad483f1d7bcec5922bb5f23fa85aed
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-10-06 21:49:20 -0700

    update board.cs

--- Assets/Scripts/ViewModelComponent/Board.cs
+++ Assets/Scripts/ViewModelComponent/Board.cs
@@ -9,6 +9,11 @@ public class Board : MonoBehaviour
 
     public Dictionary<Point, Tile> tiles = new Dictionary<Point, Tile>();
 
+    public Point min { get { return _min; }}
+    public Point max { get { return _max; }}
+    Point _min;
+    Point _max;
+
     Point[] dirs = new Point[4]
     {
         new Point(0, 1),
@@ -21,15 +26,23 @@ public class Board : MonoBehaviour
     Color defaultTileColor = new Color(1, 1, 1, 1);
 
     public void Load (LevelData data)
-    {
-        for (int i = 0; i < data.tiles.Count; ++i)
-        {
-            GameObject instance = Instantiate(tilePrefab) as GameObject;
-            Tile t = instance.GetComponent<Tile>();
-            t.Load(data.tiles[i]);
-            tiles.Add(t.pos, t);
-        }
-    }
+	{
+		_min = new Point(int.MaxValue, int.MaxValue);
+		_max = new Point(int.MinValue, int.MinValue);
+		
+		for (int i = 0; i < data.tiles.Count; ++i)
+		{
+			GameObject instance = Instantiate(tilePrefab) as GameObject;
+			Tile t = instance.GetComponent<Tile>();
+			t.Load(data.tiles[i]);
+			tiles.Add(t.pos, t);
+			
+			_min.x = Mathf.Min(_min.x, t.pos.x);
+			_min.y = Mathf.Min(_min.y, t.pos.y);
+			_max.x = Mathf.Max(_max.x, t.pos.x);
+			_max.y = Mathf.Max(_max.y, t.pos.y);
+		}
+	}
 
     public List<Tile> Search (Tile start, Func<Tile, Tile, bool> addTile)
     {
